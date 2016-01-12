278eef9fa4566698230cfd35150062032794fb3e
Andreas P <andreaseto@yahoo.gr>
2016-01-12 16:36:56 +0200

    Put todo

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/todos", function (req, res) {
 
 //GET /todos/:id
 app.get("/todos/:id", function (req, res) {
-    var todoId = parseInt(req.params.id); // το κάνουμε γιατί είναι string κανονικά
+    var todoId = parseInt(req.params.id, 10); // το κάνουμε γιατί είναι string κανονικά
     var matchedTodo = _.findWhere(todos, {id: todoId});
 
     if (matchedTodo) {
@@ -48,15 +48,47 @@ app.post("/todos", function (req, res) {
 
 // DELETE /todos/:id
 app.delete("/todos/:id", function (req, res) {
-    var todoId = parseInt(req.params.id);
+    var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos, {id: todoId});
 
     if (matchedTodo) {
-        todos = _.without(todos, matchedTodo);        
+        todos = _.without(todos, matchedTodo);
         res.json(matchedTodo);
     } else {
-        res.status(404).json({"error" : "No todo found"});        
+        res.status(404).json({"error": "No todo found"});
     }
+
+})
+
+// PUT /todos/:id
+app.put("/todos/:id", function (req, res) {
+    var todoId = parseInt(req.params.id, 10); // το κάνουμε γιατί είναι string κανονικά, το 10 είναι για το 10δικό
+    var matchedTodo = _.findWhere(todos, {id: todoId});    
+    var body = _.pick(req.body, "description", "completed"); // Επιλέγω μόνο τα έγκυρα πεδια
+    var validAttributes = {};
+    
+    if(!matchedTodo){
+        return res.status(404).send();
+    }
+    
+    
+    if(body.hasOwnProperty("completed") && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    }
+    else if(body.hasOwnProperty("completed")){
+        return res.status(400).send();
+    }
+
+    
+    if(body.hasOwnProperty("description") && _.isString(body.description) && body.description.trim().length > 0){
+        validAttributes.description = body.description;
+    }
+    else if(body.hasOwnProperty("description")){
+        return res.status(400).send();
+    }
+    
+    _.extend(matchedTodo, validAttributes); 
+    res.json(matchedTodo);
 
 })
 
