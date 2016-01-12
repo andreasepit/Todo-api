ba1716c8450e9e6ad01d7dec6b2819a355e103b4
Andreas P <andreaseto@yahoo.gr>
2016-01-12 14:39:59 +0200

    Add post todos

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var _ = require("underscore");
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 var todos = [];
 var toDoNextId = 1;
 
@@ -20,16 +21,11 @@ app.get("/todos", function (req, res) {
 //GET /todos/:id
 app.get("/todos/:id", function (req, res) {
         var todoId = parseInt(req.params.id);
-        var matchedTodo;
-        
-        todos.forEach(function(todo){
-            if(todoId === todo.id)
-                matchedTodo = todo;
-        });
+        var matchedTodo = _.findWhere(todos, {id: todoId});
         
         if(matchedTodo){
             res.json(matchedTodo);            
-        }
+        }  
         else{
             res.status(404).send();
         }               
@@ -37,9 +33,13 @@ app.get("/todos/:id", function (req, res) {
 
 //POST /todos
 app.post("/todos", function(req, res){
-    var body = req.body;
+    var body = _.pick(req.body, "description", "completed");            
     
-    console.log("Description: " + body.description);
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send(); // Ο κωδικός για το 400 είναι οτι δεν εστάλησαν δεδομένα ή σωστά δεδομένα
+    }   
+    
+    body.description = body.description.trim();
     
     body.id = toDoNextId++; 
     todos.push(body);
